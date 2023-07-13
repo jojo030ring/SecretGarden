db2ef42cc73aaa67a142558ffad5c91609eec8b6
yezi <joring517@gmail.com>
2023-07-13 10:33:53 +0900

    project setting-commit

---
+++ secretgarden/src/main/java/com/diary/secretgarden/controller/DiraryController.java
@@ -0,0 +1,13 @@
+package com.diary.secretgarden.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class DiraryController {
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
---
+++ secretgarden/src/test/java/com/diary/secretgarden/SecretgardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diary.secretgarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretgardenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
