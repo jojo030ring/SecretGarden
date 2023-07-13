791940e2ab02fb54f5faef9e1fd8183b7f577d89
yezi <mookakihi@gmail.com>
2023-07-13 21:03:46 +0900

    first commit

---
+++ src/main/java/com/yezi/secretgarden/SecretgardenApplication.java
@@ -0,0 +1,13 @@
+package com.yezi.secretgarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretgardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretgardenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yezi/secretgarden/SecretgardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yezi.secretgarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretgardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
